38a7b1bb97e9c272994ed6077c668bf0eaf974b7
kohath37 <775786325@qq.com>
2017-08-26 20:26:30 +0800

    bag

---
+++ bag/bag.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+public class bag<Item> implements Iterable<Item>{
+	@Override
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private Node first; //链表的首结点
+	private class Node
+	{
+		Item item;
+		Node next;
+	}
+	public void add(Item item)
+	{
+		Node oldfirst=first;
+		first=new Node();
+		first.item=item;
+		first.next=oldfirst;
+	}
+	private class ListIterator implements Iterator<Item>
+	{
+		private Node current=first;
+		public boolean hasNext()
+		{ return current!=null; }
+		public void remove() {}
+		public Item next()
+		{
+			Item item=current.item;
+			current=current.next;
+			return item;
+		}
+	}
+}
